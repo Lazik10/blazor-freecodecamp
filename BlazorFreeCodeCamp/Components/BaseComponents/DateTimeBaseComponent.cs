9984452a22b5a052bd05bf205361ec29842c6683
Lázik <LazikL@email.cz>
2022-11-04 13:36:53 +0100

    Component inheritance

---
+++ BlazorFreeCodeCamp/Components/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorFreeCodeCamp.Components.BaseComponents
+{
+    // We need to derive from ComponentBase
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
