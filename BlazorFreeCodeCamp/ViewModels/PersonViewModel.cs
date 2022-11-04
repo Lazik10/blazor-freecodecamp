571ef27898d0a0f8afc36e471caac3e79cf47697
Lázik <LazikL@email.cz>
2022-11-04 11:54:05 +0100

    Data binding examples
    - one way data binding
    - two way data binding
    
    Simple data binding for:
    - text
    - button
    - check box
    - radio button
    - select menu
    - list
    - table

---
+++ BlazorFreeCodeCamp/ViewModels/PersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorFreeCodeCamp.ViewModels
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public decimal Salary { get; set; }
+
+        public List<Address> Addresses = new();
+    }
+}
