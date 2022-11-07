bab28e200b603295b159b799df680c56fec0ea9f
Lázik <LazikL@email.cz>
2022-11-07 21:20:53 +0100

    Edit form
    1) User EditForm component
    2) Compose EditForm with another components like InputText, InputDate,
    InputNumber and many more
    3) Every Form has to be bind to a model class and to handle valid or
    invalid submit of a form, we set which method is used when
    4) Use bind-Value (UPPER LETTER V is IMPORTANT) to a model class
    5) Use of data annotations attributes
    6) In order to trigger the validation and display the validation
    messages we need to add
    <DataAnnotationValidator> and <DataAnnotationSummary>
    7) Create input type="submit" or <button> (it will be automatically
    submit button by default)

---
+++ BlazorFreeCodeCamp/ViewModels/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFreeCodeCamp.ViewModels
+{
+    public class FormModel
+    {
+        // 5) Use of data annotations attributes
+        [Required]
+        [MaxLength(255)]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength (255)]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
